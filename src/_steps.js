c3d695b29cf18b76e936733c3c868c89c472ee2d
nishandas <nishandas880@gmail.com>
2022-04-21 15:17:27 +0600

    all is ok

--- src/_steps.js
+++ src/_steps.js
@@ -1,4 +1,8 @@
-/* 
+/* ==========================
+------------------------------
+#Basic server setup
+-----------------------------
+==============================
 *1.create a folder (manually or mkdir)
 * 2. open command line  to that  folder
 * 3. npm init
@@ -7,13 +11,20 @@
 * -----------------
 * For express / node server 
 ----------------------*
-*1.require express create app variable
-*declare port
-*set app.get('/')
-*listen to port
-*node index.js
-*check your browser for that port
-*
-*
+* 1.require express
+* 2. create app variable
+* 3.declare port
+* 4.set app.get('/')
+* 5.listen to port
+* 6.node index.js
+* 7.check your browser for that port
+*===================
+*get
+*====================
+*1. cors middleware
+*========================
+*post 
+*====================
+1.shortcat =app.use(express.json() )use
 
  */

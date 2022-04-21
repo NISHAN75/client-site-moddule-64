3f3844b753e06e9b2b5f27c6f496667144e0bbec
nishandas <nishandas880@gmail.com>
2022-04-21 13:22:54 +0600

    client site ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+
+    // post data to server
+    fetch("http://localhost:5000/user", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const newUser=[...users,data];
+        setUsers(newUser);
+        console.log(data)
+      })
+      
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>my own dataBage:{users.length}</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" placeholder="Enter Your Name" />
+        <input type="email" name="email" id="" placeholder="Enter Your email" />
+        <input type="submit" value="Add User" />
+      </form>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>name:{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/_steps.js
@@ -0,0 +1,19 @@
+/* 
+*1.create a folder (manually or mkdir)
+* 2. open command line  to that  folder
+* 3. npm init
+* 4.npm install express
+* 5.create index.js
+* -----------------
+* For express / node server 
+----------------------*
+*1.require express create app variable
+*declare port
+*set app.get('/')
+*listen to port
+*node index.js
+*check your browser for that port
+*
+*
+
+ */
